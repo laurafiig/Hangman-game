03bda4b0b58e5790c21de1345ab3576be904b25b
Laura Fiig <Laura Fiig>
2016-11-03 22:45:06 -0500

    functionality added to check guesses against answer.  final answer check still broken.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,62 @@
-//note to self - re sketch out the nesting of the various conditionals
-//	first	check if letter already used, then iterate guess count and letters guessed
-//	second	check if answer is correct, replace on screen.  multiple places if neccesary
-//	third	check if answer is complete, winner text and music, hit any key to restart??
 
 //Array of correct answers
 var answers=["Washington","Adams","Jefferson","Madison","Lincoln","Roosevelt","Kennedy","Reagan","Clinton","Obama"]
-
-// other variables needed
-
+// starter values for variable display
 var numWins = 0
 var remGuess = 15
 var letters =[]
-
 //select random answer, convert to proper length array of spaces, populate word field
-//!!!!still leaves me with the commas!!!!
-
 var answer = answers[Math.floor(Math.random()*answers.length)];
+//convert to underscores
 var ansUs = answer.replace(/./gi, "_");
+//split underscores into array
 var ansUsArray = ansUs.split("");
+// combine array into string (don't need here)
+//var ansDisplay = ansUsArray.join("")
+// convert answer to uppercase
 var ansCap = answer.toUpperCase();
-var ansArray = ansCap.split("");
-
-// poulate fields in html with starting values
-document.getElementById("word").innerHTML= ansUsArray;
+//split answer into array, not needed for now
+//var ansArray = ansCap.split("");
+// populate fields in html with starting values
+document.getElementById("word").innerHTML= ansUs;
 document.getElementById("guesses").innerHTML = remGuess;
 document.getElementById("wins").innerHTML = numWins;
-
 //actions when key is clicked:
-//  check if already used
-//	add to letters used array
-//	subtract from guesses remaining total
+
 //	check array for correct letter
 //  replace if correct
 
-//make input capital letter
+//make key input capital letter
 document.onkeyup = function(guess) {
 	var guessCap = String.fromCharCode(guess.keyCode).toUpperCase();
-	
-//check if used
+//check if guessed previously
 var chkUsed = letters.indexOf(guessCap);
-
-if (chkUsed === -1) {
+if (chkUsed === -1) {              
+	//check if correct guess
+	if (ansCap.includes(guessCap) === true) {
+			//check for game won
+			if (ansCap === ansUs) {
+				//winner graphic
+				document.getElementById("youwin1").innerHTML = "CONGRATS";
+				document.getElementById("youwin2").innerHTML = "You Win!";
+				document.getElementById("youwin3").innerHTML = "The answer is:";
+				document.getElementById("youwin4").innerHTML = ansCap;
+			}
+		//replace in current word array
+		//search answer for guessed letter
+		var pos = [];
+		for(var i=0; i<ansCap.length;i++) {
+    		if (ansCap[i] === guessCap) pos.push(i);
+		}
+		//replace underscored in word and re-pop
+		for(i=0; i<pos.length; i++) {
+			ansUsArray[pos[i]] = guessCap
+		}
+		var ansUs = ansUsArray.join("");
+		document.getElementById("word").innerHTML= ansUs;
+		console.log("answer is " + ansCap);
+		console.log("guess is " + ansUs);
+	}
 
 	//add to letters used array
 	letters.push(guessCap);
@@ -50,16 +66,14 @@ if (chkUsed === -1) {
 	remGuess--;
 	document.getElementById("guesses").innerHTML = remGuess;
 
-} else alert ("You guessed this letter already.  Please try again.");	
+//already guessed alert
+} else { alert ("You guessed this letter already.  Please try again.");	 
+
+}
 
 }	
 
-//check answer
 
-//similar to??
-//if (myFarm[i].charAt(0) === "c" || myFarm[i].charAt(0) === "o") {
-
-//replace on screen
 	
 //need stop when out of guesses
 
